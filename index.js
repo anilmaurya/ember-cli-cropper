6850e0e8bf612b8137d109e632431dfb8f929bbe
Anil Maurya <anil@joshsoftware.com>
2015-03-23 21:30:48 +0530

    Add cropper dependency and update package json

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-cropper'
+  name: 'ember-cli-cropper',
+
+  included: function(app){
+    this._super.included(app);
+
+    this.app.import(app.bowerDirectory + '/cropper/dist/cropper.js');
+    this.app.import(app.bowerDirectory + '/cropper/dist/cropper.css');
+  }
 };
